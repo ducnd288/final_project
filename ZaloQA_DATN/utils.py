f0361284d586a13be5d422f2ab5184d7a4ae0f9f
DN <ducyou1992d@gmail.com>
2020-04-27 22:24:49 +0700

    m

--- ZaloQA_DATN/utils.py
+++ ZaloQA_DATN/utils.py
@@ -1,7 +1,7 @@
 import logging
 
 def init_logger():
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+    logging.basicConfig(filename='log.txt', filemode='a', format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S',
                         level=logging.INFO)
                     
